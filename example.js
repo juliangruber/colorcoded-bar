e25cf4ccb6e1306d3d4c4da04957cda323656864
Julian Gruber <julian@juliangruber.com>
2015-10-07 23:24:54 +0200

    example

--- example.js
+++ example.js
@@ -23,3 +23,14 @@ var Bar = require('./');
   }
   document.body.appendChild(bar.render({ height: 200 }));
 })();
+
+(function(){
+  var bar = new Bar();
+  var height = 500;
+  for (var i = 0; i < height; i++) {
+    var color = 'hsl(' + Math.round(i / height * 360) + ', 100%, 50%)';
+    bar.set(i, color);
+  }
+  document.body.appendChild(bar.render({ height: height }));
+})();
+
